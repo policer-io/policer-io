ad2e6d147dc827e716c018483b6fe7b2b0ff90e9
Timon Heinis <timon@embrio.tech>
2024-07-17 10:07:21 +0000

    refactor: rename db seeding files

---
+++ seeds/policer-db/db_init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongorestore --db=$MONGO_INITDB_DATABASE --authenticationDatabase=admin -u=$MONGO_INITDB_ROOT_USERNAME -p=$MONGO_INITDB_ROOT_PASSWORD --archive=/docker-entrypoint-initdb.d/seed.dump
